ba87f991585e15224a322f2a119b308082ffdb14
Pawel Brudniak <pawelbrudniak@pm.me>
2026-06-14 16:37:33 +0200

    Switch colorscheme to Gruvbox

--- lua/paul/plugins/colorscheme.lua
+++ lua/paul/plugins/colorscheme.lua
@@ -1,15 +1,25 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                transparent_background = false,
-            })
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true,
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = false,
+					comments = true,
+					operators = false,
+					folds = true,
+				},
+				contrast = "hard", -- "soft", "medium", "hard"
+				transparent_mode = false,
+			})
 
-            vim.cmd.colorscheme("catppuccin")
-        end,
-    },
+			vim.o.background = "dark"
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
 }
